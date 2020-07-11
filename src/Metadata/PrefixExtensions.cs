3fdb88da692da6abf9351ceedd67dad42563c4a6
David Driscoll <david.driscoll@gmail.com>
2020-07-11 14:11:28 -0400

    Added metadata package

---
+++ src/Metadata/PrefixExtensions.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Rocket.Surgery.LaunchPad.Metadata
+{
+    /// <summary>
+    /// PrefixExtensions.
+    /// </summary>
+    internal static class PrefixExtensions
+    {
+        /// <summary>
+        /// Infers the specified instance.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="provider">The provider.</param>
+        /// <param name="instance">The instance.</param>
+        internal static void Infer<T>(this AssemblyMetadataProvider provider, T instance)
+        {
+            foreach (var property in instance!.GetType().GetTypeInfo().DeclaredProperties)
+            {
+                // simple props only
+                if (
+                    property.PropertyType.GetTypeInfo().IsPrimitive
+                    || property.PropertyType.GetTypeInfo().IsEnum
+                    || property.PropertyType == typeof(string))
+                {
+                    var prefix = property.GetCustomAttribute<PrefixAttribute>()?.Key ?? string.Empty;
+                    var value = provider.GetValue(prefix + property.Name).FirstOrDefault();
+                    if (!string.IsNullOrWhiteSpace(value))
+                    {
+                        property.SetValue(instance, Convert.ChangeType(value, property.PropertyType));
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ test/Metadata.Tests/GitVersionTests.cs
@@ -0,0 +1,115 @@
+using System;
+using System.Linq;
+using System.Reflection;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Rocket.Surgery.Conventions;
+using Rocket.Surgery.Extensions.Testing;
+using Rocket.Surgery.LaunchPad.Metadata;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Metadata.Tests
+{
+    public class GitVersionTests : AutoFakeTest
+    {
+        public GitVersionTests(ITestOutputHelper outputHelper) : base(outputHelper) { }
+
+        [Fact(Skip = "Disabled for CI")]
+        public void ReturnsInformationForVersionedAssembly()
+        {
+            var version = GitVersion.For(typeof(GitVersion).GetTypeInfo().Assembly);
+
+            version.Should().NotBeNull();
+            version.SemVer.Should().NotBeNullOrWhiteSpace();
+        }
+
+        [Fact]
+        public void EqualToEachOther()
+        {
+            var version = GitVersion.For(typeof(GitVersion).GetTypeInfo().Assembly);
+            var version2 = GitVersion.For(typeof(GitVersion).GetTypeInfo().Assembly);
+            version.Should().NotBeNull();
+            version.Should().Be(version2);
+        }
+
+        [Fact]
+        public void NotHaveAVersionForNonVersionedAssembly()
+        {
+            var version = GitVersion.For(typeof(string).GetTypeInfo().Assembly);
+            version.HasVersion.Should().BeFalse();
+        }
+
+        [Fact(Skip = "Disabled for CI")]
+        public void ForTypeInfo()
+        {
+            var version = GitVersion.For(typeof(GitVersion).GetTypeInfo());
+
+            version.Should().NotBeNull();
+            version.HasVersion.Should().BeTrue();
+        }
+
+        [Fact(Skip = "Disabled for CI")]
+        public void ForType()
+        {
+            var version = GitVersion.For(typeof(GitVersion));
+
+            version.Should().NotBeNull();
+            version.HasVersion.Should().BeTrue();
+        }
+
+        [Fact]
+        public void ForManyTypesAndReduce()
+        {
+            var version = GitVersion.For(typeof(GitVersion), typeof(GitVersion));
+
+            version.Should().NotBeNull();
+            version.Should().HaveCount(1);
+        }
+
+        [Fact]
+        public void ForManyTypeInfosAndReduce()
+        {
+            var version = GitVersion.For(typeof(GitVersion).GetTypeInfo(), typeof(GitVersion).GetTypeInfo());
+
+            version.Should().NotBeNull();
+            version.Should().HaveCount(1);
+        }
+
+        [Fact]
+        public void ForManyAssembliesAndReduce()
+        {
+            var version = GitVersion.For(typeof(GitVersion).GetTypeInfo().Assembly, typeof(GitVersion).GetTypeInfo().Assembly);
+
+            version.Should().NotBeNull();
+            version.Should().HaveCount(1);
+        }
+
+        [Fact]
+        public void ForManyTypesAndReduceEnumerable()
+        {
+            var version = GitVersion.For(new[] { typeof(GitVersion), typeof(GitVersion) }.AsEnumerable());
+
+            version.Should().NotBeNull();
+            version.Should().HaveCount(1);
+        }
+
+        [Fact]
+        public void ForManyTypeInfosAndReduceEnumerable()
+        {
+            var version = GitVersion.For(new[] { typeof(GitVersion).GetTypeInfo(), typeof(GitVersion).GetTypeInfo() }.AsEnumerable());
+
+            version.Should().NotBeNull();
+            version.Should().HaveCount(1);
+        }
+
+        [Fact]
+        public void ForManyAssembliesAndReduceEnumerable()
+        {
+            var version = GitVersion.For(new[] { typeof(GitVersion).GetTypeInfo().Assembly, typeof(GitVersion).GetTypeInfo().Assembly }.AsEnumerable());
+
+            version.Should().NotBeNull();
+            version.Should().HaveCount(1);
+        }
+    }
+}
