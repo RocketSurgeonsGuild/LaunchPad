91e8ee2c4ca918ef6418126e36c21b20da140ab0
David Driscoll <david.driscoll@gmail.com>
2023-05-08 23:18:21 -0400

    Updated build

--- .build/Build.CI.cs
+++ .build/Build.CI.cs
@@ -67,7 +67,7 @@ public partial class Pipeline
            .ExcludeRepositoryConfigurationFiles()
            .AddNugetPublish()
            .Jobs.OfType<RocketSurgeonsGithubActionsJob>()
-           .First(z => z.Name == "Build")
+           .First(z => z.Name.Equals("build", StringComparison.OrdinalIgnoreCase))
            .UseDotNetSdks("6.0", "7.0")
            .AddNuGetCache()
             // .ConfigureForGitVersion()
