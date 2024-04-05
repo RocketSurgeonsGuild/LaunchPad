4338b8551610fe04b7ab64a14a209277621bd769
Rocket Understudy <33589210+rsg-bot@users.noreply.github.com>
2024-04-04 22:27:07 -0400

    Automatically linting code

--- src/Analyzers/ContextExtensions.cs
+++ src/Analyzers/ContextExtensions.cs
@@ -11,38 +11,6 @@ namespace Rocket.Surgery.LaunchPad.Analyzers;
 
 internal static class Helpers
 {
-
-    internal static AttributeListSyntax CompilerGeneratedAttributes =
-        AttributeList(
-            SeparatedList(
-                [
-                    Attribute(ParseName("System.CodeDom.Compiler.GeneratedCode"))
-                       .WithArgumentList(
-                            AttributeArgumentList(
-                                SeparatedList(
-                                    [
-                                        AttributeArgument(
-                                            LiteralExpression(
-                                                SyntaxKind.StringLiteralExpression,
-                                                Literal(typeof(Helpers).Assembly.GetName().Name)
-                                            )
-                                        ),
-                                        AttributeArgument(
-                                            LiteralExpression(
-                                                SyntaxKind.StringLiteralExpression,
-                                                Literal(typeof(Helpers).Assembly.GetCustomAttribute<AssemblyFileVersionAttribute>()?.Version ?? "generated")
-                                            )
-                                        )
-                                    ]
-                                )
-                            )
-                        ),
-                    Attribute(ParseName("System.Runtime.CompilerServices.CompilerGenerated")),
-                    Attribute(ParseName("System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage"))
-                ]
-            )
-        );
-
     public static void AddSourceRelativeTo(this SourceProductionContext context, TypeDeclarationSyntax declaration, string suffix, SourceText sourceText)
     {
         context.AddSource(
