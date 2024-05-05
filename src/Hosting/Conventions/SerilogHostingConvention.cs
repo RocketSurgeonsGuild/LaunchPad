3f2ebad2177a97f7e99cacdb5fa5115a621e9618
Rocket Understudy <33589210+rsg-bot@users.noreply.github.com>
2024-05-05 13:58:53 -0400

    Automatically linting code

--- src/Hosting/Conventions/SerilogHostingConvention.cs
+++ src/Hosting/Conventions/SerilogHostingConvention.cs
@@ -4,9 +4,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Rocket.Surgery.Conventions;
 using Rocket.Surgery.Conventions.Hosting;
-using Rocket.Surgery.Conventions.Logging;
 using Rocket.Surgery.Hosting;
-using Rocket.Surgery.LaunchPad.Serilog;
 using Serilog;
 using Serilog.Core;
 using Serilog.Events;
@@ -25,6 +23,33 @@ namespace Rocket.Surgery.LaunchPad.Hosting.Conventions;
 [ExportConvention]
 public class SerilogHostingConvention : IHostApplicationConvention, IHostCreatedConvention<IHost>
 {
+    private void CustomAddSerilog(
+        IServiceCollection collection,
+        Action<IServiceProvider, LoggerConfiguration> configureLogger
+    )
+    {
+        collection.AddSingleton(new LoggerProviderCollection());
+        collection.AddSingleton(
+            services =>
+            {
+                var loggerConfiguration = new LoggerConfiguration();
+                loggerConfiguration.WriteTo.Providers(services.GetRequiredService<LoggerProviderCollection>());
+                configureLogger(services, loggerConfiguration);
+                return loggerConfiguration.CreateLogger();
+            }
+        );
+        collection.AddSingleton<ILogger>(services => services.GetRequiredService<Logger>().ForContext(new NullEnricher()));
+        collection.AddSingleton<ILoggerFactory>(
+            services => new SerilogLoggerFactory(
+                services.GetRequiredService<Logger>(),
+                true,
+                services.GetRequiredService<LoggerProviderCollection>()
+            )
+        );
+        collection.AddSingleton(services => new DiagnosticContext(services.GetRequiredService<Logger>()));
+        collection.AddSingleton<IDiagnosticContext>(services => services.GetRequiredService<DiagnosticContext>());
+    }
+
     /// <inheritdoc />
     public void Register(IConventionContext context, IHostApplicationBuilder builder)
     {
@@ -64,48 +89,24 @@ public class SerilogHostingConvention : IHostApplicationConvention, IHostCreated
             builder.Services.AddSingleton(context.Get<ILoggerFactory>()!);
     }
 
-    private void CustomAddSerilog(
-        IServiceCollection collection,
-        Action<IServiceProvider, LoggerConfiguration> configureLogger
-    )
+    /// <inheritdoc />
+    public void Register(IConventionContext context, IHost host)
     {
-        collection.AddSingleton(new LoggerProviderCollection());
-        collection.AddSingleton(
-            services =>
-            {
-                var loggerConfiguration = new LoggerConfiguration();
-                loggerConfiguration.WriteTo.Providers(services.GetRequiredService<LoggerProviderCollection>());
-                configureLogger(services, loggerConfiguration);
-                return loggerConfiguration.CreateLogger();
-            }
-        );
-        collection.AddSingleton<ILogger>(services => services.GetRequiredService<Logger>().ForContext(new NullEnricher()));
-        collection.AddSingleton<ILoggerFactory>(
-            services => new SerilogLoggerFactory(
-                services.GetRequiredService<Logger>(),
-                true,
-                services.GetRequiredService<LoggerProviderCollection>()
-            )
-        );
-        collection.AddSingleton(services => new DiagnosticContext(services.GetRequiredService<Logger>()));
-        collection.AddSingleton<IDiagnosticContext>(services => services.GetRequiredService<DiagnosticContext>());
+        host
+           .Services
+           .GetServices<ILoggerProvider>()
+           .Aggregate(
+                host.Services.GetRequiredService<LoggerProviderCollection>(),
+                (factory, loggerProvider) =>
+                {
+                    factory.AddProvider(loggerProvider);
+                    return factory;
+                }
+            );
     }
 
     private class NullEnricher : ILogEventEnricher
     {
         public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory) { }
     }
-
-    /// <inheritdoc />
-    public void Register(IConventionContext context, IHost host) => host
-                                                                   .Services
-                                                                   .GetServices<ILoggerProvider>()
-                                                                   .Aggregate(
-                                                                        host.Services.GetRequiredService<LoggerProviderCollection>(),
-                                                                        (factory, loggerProvider) =>
-                                                                        {
-                                                                            factory.AddProvider(loggerProvider);
-                                                                            return factory;
-                                                                        }
-                                                                    );
 }
