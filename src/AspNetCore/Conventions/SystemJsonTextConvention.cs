bb4ccd569101cc460a73d5a23b9dd5deae70600b
David Driscoll <david.driscoll@gmail.com>
2024-08-21 18:55:31 -0400

    Added json settings for httpjsonoptions (#2165)

--- src/AspNetCore/Conventions/SystemJsonTextConvention.cs
+++ src/AspNetCore/Conventions/SystemJsonTextConvention.cs
@@ -1,10 +1,11 @@
 using System.Text.Json;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Rocket.Surgery.Conventions;
 using Rocket.Surgery.Conventions.DependencyInjection;
 using Rocket.Surgery.LaunchPad.Foundation;
+using MvcJsonOptions = Microsoft.AspNetCore.Mvc.JsonOptions;
+using HttpJsonOptions = Microsoft.AspNetCore.Http.Json.JsonOptions;
 
 namespace Rocket.Surgery.LaunchPad.AspNetCore.Conventions;
 
@@ -41,11 +42,18 @@ public class SystemJsonTextConvention : IServiceConvention
         ArgumentNullException.ThrowIfNull(context);
 
         services
-           .AddOptions<JsonOptions>()
+           .AddOptions<MvcJsonOptions>()
            .Configure<IServiceProvider>(
                 (options, provider) => ActivatorUtilities
                                       .CreateInstance<ExistingValueOptionsFactory<JsonSerializerOptions>>(provider, options.JsonSerializerOptions)
-                                      .Create(nameof(JsonOptions))
+                                      .Create(nameof(MvcJsonOptions))
+            );
+        services
+           .AddOptions<HttpJsonOptions>()
+           .Configure<IServiceProvider>(
+                (options, provider) => ActivatorUtilities
+                                      .CreateInstance<ExistingValueOptionsFactory<JsonSerializerOptions>>(provider, options.SerializerOptions)
+                                      .Create(nameof(MvcJsonOptions))
             );
     }
 }
